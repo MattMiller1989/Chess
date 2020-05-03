3c3f07fe64bf1ef34387634be6a4b26cc61f82b1
Matt Miller <mattmiller0989@gmail.com>
2020-05-03 16:58:57 -0500

    Added save and load functions

--- Game_runner.rb
+++ Game_runner.rb
@@ -1,58 +1,79 @@
 require_relative 'Pieces'
 require_relative 'Move'
 require_relative 'Chess'
+require 'yaml'
 
 class Game_runner
     attr_reader :my_board
     def initialize(board=nil)
         @move_list=[]
         if board==nil
-            @my_board=Game_board.new
-            @my_board.print_board
-            play_game  
+            start_game
+            # @my_board=Game_board.new
+            # @my_board.print_board
+            # play_game  
             
         else
             @my_board=Game_board.new(board)
+            @my_board.print_board
+            play_game  
         end
         
              
     end
+    def start_game
+        
+        puts "Do you want to load a previous game? [y/n]"
+        
+        response=gets.chomp
+        if response=='y'
+            load_game
+            @my_board.print_board
+            play_game
+        else
+            @my_board=Game_board.new
+            @turn='white'
+            @my_board.print_board
+            play_game  
+        end
+        
+    end
     def play_game
         
-        turn='white'
+        
         game_over=false
         # @my_board.print_board
         puts "Let's Play Chess!! Here are some examples of valid inputs: a2a4 a5a2 b4h2"
-        
+        puts "A few other possible commands: save and forfeit"
         check =false
         
         while !game_over
-
+            save_game
                 
 
-                curr_move=get_move(turn)
+                curr_move=get_move(@turn)
 
-                # check=@my_board.in_check?(turn[0],curr_king.x,curr_king.y)
-
-                # puts "check #{check}"
-                # while check
-                #     puts "You are in check!!"
-                    
-                # check=@my_board.in_check?(turn[0],curr_king.x,curr_king.y)
-               
+                if curr_move=='save'
+                    game_over=true
+                    save_game
+                    break
+                end
+                if curr_move=='forfeit'
+                    game_over=true
+                    declare_winner
+                    break
+                end
                 
-                
-            
                 
                 @my_board.move_piece(curr_move)
                 @my_board.print_board
                 puts @move_list.last
 
 
-                if turn== 'white'
-                    turn= 'black'
+                if @turn== 'white'
+                    @turn= 'black'
                 else
-                    turn= 'white'
+                    @turn= 'white'
                 end
                 
         end
@@ -61,12 +82,17 @@ class Game_runner
     def get_move(team)
             valid_move=false
             while ! valid_move
+                puts "enter 'save' to save or 'forfeit' to end the game and codede defeat"
                 puts "team #{team} please enter your move: "
                 move_in=gets.chomp
                 
                 
                 if move_in.match(/^([a-h][1-8]){2}$/) != nil
                     valid_in=true
+                elsif move_in.match(/^(save)$/) !=nil
+                    return 'save'
+                elsif move_in.match(/^(forfeit)$/) !=nil
+                    return 'forfeit'
                 else
                     valid_in=false
                 end
@@ -146,6 +172,37 @@ class Game_runner
         # puts "#{my_move} #{check}"
         return check
 
+    end
+    def declare_winner
+        if @turn == 'white'
+            winner='black'
+        else
+            winner='white'
+        end
+    
+        puts "#{winner} has won the game!!!11!!"
+    end
+
+    def save_game()
+        data={
+            "board"        => @my_board,
+            "move_list"    => @move_list,
+            "turn"         => @turn
+        }
+        yaml = YAML.dump(data)
+        game_file = File.open("save.txt","w")
+        game_file.write(yaml)
+        
+    end
+
+    def load_game
+        
+        yaml = YAML.load(File.open("save.txt"))
+        
+        @my_board               = yaml["board"]
+        @move_list              = yaml["move_list"]
+        @turn                   = yaml["turn"]
+        
     end
 end
 
